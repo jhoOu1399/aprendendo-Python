59243fbeec8486dff91d900deb822ec5e1536053
Johnny Santos <129534145+jhoOu1399@users.noreply.github.com>
2023-05-11 22:41:39 -0300

    criando o escopo adivinhacao.py

---
+++ adivinhacao.py
@@ -0,0 +1,18 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 10
+
+chute_srt = input("Digite o seu número: ")
+
+print("Você digitou: ", chute_srt) 
+
+chute = int(chute_srt)
+
+if(numero_secreto == chute):
+  print("Parábens, você acertou!")
+else:
+  print("Você errou, que pena...")
+ 
+print("Fim de jogo")
